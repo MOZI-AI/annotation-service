33a5301c7f768b4bdba4d31f20611280a84eab8f
Abdulrahman Semrie <hsamireh@gmail.com>
2019-01-30 18:25:48 +0300

    Fixed a bug with install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ snet_daemon_v=0.1.5
 
 # apt install tar
 if [ ! -d snet-daemon-v$snet_daemon_v ] ; then
+    mkdir snet-daemon-v$snet_daemon_v
 	echo "Downloading snet-daemon"
 	wget https://github.com/singnet/snet-daemon/releases/download/v$snet_daemon_v/snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
 	tar -xzf snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz -C snet-daemon-v$snet_daemon_v --strip-components 1
