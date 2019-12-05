54be4f5e9e01c3794a177e84a68a6b47fa85559a
Abdulrahman Semrie <hsamireh@gmail.com>
2019-12-05 17:44:33 +0300

    Fix the PLN_RULE path

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ CSV_TEST_FOLDER = os.path.join(TEST_FOLDER,"csv")
 
 RESULT_DIR = "/root/result/"
 
-PLN_RULE = "scheme/annotation/pln_rule.scm"
+PLN_RULE = os.path.join(PROJECT_ROOT, "scheme/annotation/pln_rule.scm")
 
 try:
     DATASET_PATHs = [os.path.join(DATASET_FOLDER, dataset) for dataset in os.listdir(DATASET_FOLDER) if
